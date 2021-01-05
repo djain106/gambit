270beb3786579d6f88d8d59e9119eb99d83c9782
Daksh Jain <dakshbjain@gmail.com>
2021-01-05 18:38:20 -0500

    added buttons, might change method

--- src/pages/roulette/RouletteTable.js
+++ src/pages/roulette/RouletteTable.js
@@ -3,9 +3,74 @@ import './Roulette.css'
 
 function RouletteTable(
 ) {
+    let num = 1;
     return (
-        <div className="table">
-            <button>Test</button>
+        <div className="betTable">
+            <div>
+                <button className="zero green">0</button>
+                <div className="section">
+                    <button className="red">1</button>
+                    <button className="black">2</button>
+                    <button className="red">3</button>
+                    <button className="black">4</button>
+                    <button className="red">5</button>
+                    <button className="black">6</button>
+                    <button className="red">7</button>
+                    <button className="black">8</button>
+                    <button className="red">9</button>
+                    <button className="black">10</button>
+                    <button className="black">11</button>
+                    <button className="red">12</button>
+                </div>
+                <div className="section">
+                    <button className="black">13</button>
+                    <button className="red">14</button>
+                    <button className="black">15</button>
+                    <button className="red">16</button>
+                    <button className="black">17</button>
+                    <button className="red">18</button>
+                    <button className="red">19</button>
+                    <button className="black">20</button>
+                    <button className="red">21</button>
+                    <button className="black">22</button>
+                    <button className="red">23</button>
+                    <button className="black">24</button>
+                </div>
+                <div className="section">
+                    <button className="red">25</button>
+                    <button className="black">26</button>
+                    <button className="red">27</button>
+                    <button className="black">28</button>
+                    <button className="black">29</button>
+                    <button className="red">30</button>
+                    <button className="black">31</button>
+                    <button className="red">32</button>
+                    <button className="black">33</button>
+                    <button className="red">34</button>
+                    <button className="black">35</button>
+                    <button className="red">36</button>
+                </div>
+                <div className="section">
+                    <button className="green">2-1</button>
+                    <button className="green">2-1</button>
+                    <button className="green">2-1</button>
+                </div>
+            </div>
+            <div className="extraButtons">
+                <div className="twelveButtons">
+                    <button className="twelve"><span>1st12</span></button>
+                    <button className="twelve"><span>2nd12</span></button>
+                    <button className="twelve"><span>3rd12</span></button>
+                </div>
+                <div className="edgeButtons">
+                    <button><span>1-18</span></button>
+                    <button><span>EVEN</span></button>
+                    <button style={{ backgroundColor: "red" }}></button>
+                    <button style={{ backgroundColor: "black" }}></button>
+                    <button><span>ODD</span></button>
+                    <button><span>19-36</span></button>
+                </div>
+            </div>
         </div>
     )
 }
