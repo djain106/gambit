e1a4d88cbf597a43af1df92e0c5a81cad2963d00
Daksh Jain <dakshbjain@gmail.com>
2021-01-05 00:31:54 -0500

    added numbers to wheel

--- src/pages/roulette/RouletteWheel.js
+++ src/pages/roulette/RouletteWheel.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Roulette.css'
 
-const WHEEL_NUMBERS = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 28, 12, 35, 3, 26];
+const WHEEL_NUMBERS = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
 const share = 360 / WHEEL_NUMBERS.length;
 
 function RouletteWheel(props) {
@@ -9,9 +9,10 @@ function RouletteWheel(props) {
         <div className="circle">
             {WHEEL_NUMBERS.map((num, index) =>
                 <div key={index} className="slice" style={{
-                    transform: `rotate(${(index * share) - (share / 2)}deg) skewY(${share - 90}deg)`
+                    transform: `rotate(${((index * share) - (share / 2))}deg) skewY(${(share - 90)}deg)`
                 }}>
-                    <p style={{ transform: `skewY(${-(share - 90)}) rotate(${share / 2}deg)` }}>
+                    <p className="text"
+                        style={{ '--share': `${share}deg` }}>
                         {num}
                     </p>
                 </div>
