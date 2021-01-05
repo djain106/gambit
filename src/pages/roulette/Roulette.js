ec4a5cc2287369626a6cce851e192f70981e12bf
Daksh Jain <dakshbjain@gmail.com>
2021-01-05 13:08:46 -0500

    added random spinning on roulette wheel

--- src/pages/roulette/Roulette.js
+++ src/pages/roulette/Roulette.js
@@ -1,24 +1,30 @@
 import React, { useState } from 'react'
-import { TweenLite, TweenMax, Power2 } from 'gsap'
 import RouletteWheel from './RouletteWheel'
 import RouletteTable from './RouletteTable';
 
+const WHEEL_NUMBERS = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
+const share = 360 / WHEEL_NUMBERS.length;
+
 function Roulette() {
     const [spinning, setSpinning] = useState(false);
+    const [selectedIndex, setSelected] = useState();
 
     function spin() {
+        setSelected(Math.floor(Math.random() * Math.floor(WHEEL_NUMBERS.length)));
         setSpinning(true);
     }
 
     function endSpin() {
+        console.log(selectedIndex)
         setSpinning(false);
+        setSelected(undefined);
     }
 
     return (
         <div className="border">
             <div className="table">
                 <div className="pointer"></div>
-                <RouletteWheel spinning={spinning} />
+                <RouletteWheel winner={selectedIndex} numbers={WHEEL_NUMBERS} share={share} spinning={spinning} />
                 <RouletteTable spin={spin} endSpin={endSpin} />
             </div>
         </div>
--- src/pages/roulette/RouletteWheel.js
+++ src/pages/roulette/RouletteWheel.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './Roulette.css'
 
-const WHEEL_NUMBERS = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
-const share = 360 / WHEEL_NUMBERS.length;
-
 function RouletteWheel(props) {
+    const share = props.share;
+    const winningIndex = props.winner;
+    let startSpin = `
+    @keyframes spinning {
+        from { transform: rotate(0); }
+        to { transform: rotate(${(winningIndex * share) + 360}deg); }
+    }
+    `;
     return (
-        <div className={`circle${props.spinning ? " spinning" : ""}`} >
-            {WHEEL_NUMBERS.map((num, index) =>
+        <div className={`circle${props.spinning ? " spinning" : ""}`}>
+            {props.numbers.map((num, index) =>
                 <div key={index} className="slice" style={{
                     transform: `rotate(${((index * share) - (share / 2))}deg) skewY(${(share - 90)}deg)`
                 }}>
@@ -16,6 +21,7 @@ function RouletteWheel(props) {
                     </p>
                 </div>
             )}
+            <style children={startSpin} />
             <div className="innerCircle"></div>
         </div>
     )
