7433f859160e4f7b95cfc8ff081a85c070168ca0
Daksh Jain <dakshbjain@gmail.com>
2021-01-28 23:56:36 -0500

    small changes to slots animation

--- src/pages/slots/Carousel.js
+++ src/pages/slots/Carousel.js
@@ -8,7 +8,7 @@ function Carousel(props) {
             <div id={`ring-${props.ringId}`}>
                 {
                     numbers.map((num, i) =>
-                        <div key={num} className="carousel__cell" style={{ transform: `rotateX(${i * (360 / numbers.length)}deg) translateZ(150px)` }}><p>{num}</p></div>
+                        <div key={num} className="carousel__cell" style={{ transform: `rotateX(${i * (360 / numbers.length)}deg) translateZ(90px)` }}><p>{num}</p></div>
                     )
                 }
             </div>
